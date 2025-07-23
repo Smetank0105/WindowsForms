8a9e353d15e85ab049bef9bc3a422c8969acf1ad
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2025-07-23 20:26:55 +0700

    FormBorderStyle = FixedToolWindow

--- WindowsForms/ChooseFont.Designer.cs
+++ WindowsForms/ChooseFont.Designer.cs
@@ -125,6 +125,7 @@ namespace WindowsForms
 			this.Controls.Add(this.comboBoxChooseFont);
 			this.Controls.Add(this.lblChooseFont);
 			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "ChooseFont";
 			this.ShowInTaskbar = false;
